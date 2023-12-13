efc6f37d869943d746dac51de7bd2716338f58e6
johnSnow <54940899@qq.com>
2023-12-13 11:54:27 +0800

    修改简历

--- src/main.js
+++ src/main.js
@@ -11,14 +11,14 @@ var option = {
               color: '#fff',
               backgroundColor: '#999',
               borderRadius: 3,
-              padding: [3, 5]
+              padding: [0, 5]
           }
       },
       indicator: [
         { name: '静态页面', max: 100},
         { name: '编程基础', max: 100},
         { name: 'Vue', max: 100},
-        { name: 'React', max: 100},
+        { name: 'TypeScript', max: 100},
         { name: '项目开发', max: 100},
         { name: '沟通能力', max: 100}
       ]
